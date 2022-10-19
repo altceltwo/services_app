46d36f15504b6ec7430abd3db2d88a4a42b11360
lalo0724 <auxiliar.programacion7@alternativa19delsur.com>
2022-10-19 11:22:29 -0500

    Cambios en controllers interacción con appmovil

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -34,32 +34,39 @@ class AuthenticationController extends Controller
 
     public function register(Request $request){
         $name = $request['name'];
-        $data = DB::connection('app_mobile')->table('users')->insert([
-            'name'=> $name
-        ]);
+        // $data = DB::connection('app_mobile')->table('users')->insert([
+        //     'name'=> $name
+        // ]);
         $email = $request['email'];
         $phone = $request['phone'];
         $password = $request['password'];
-        $passwordConfir = $request['passwordConfir'];
+        $passwordConfirm = $request['passwordConfirm'];
 
-
-        if($password == $passwordConfir){
+        if($password == $passwordConfirm){
 
             $NumeroExistente = DB::connection('app_mobile')->table('users')
             ->select('phone')
             ->where('phone', '=', $phone)->exists();
 
             if ($NumeroExistente == 1){
-                return response()->json(['http_code'=>'400','message'=>'Número se encuentra registrado']);
+                return response()->json(['http_code'=>'400','message'=>'Número se encuentra registrado'],400);
             }else{
                 $newPassword = Hash::make($password);
                 $data = DB::connection('app_mobile')->table('users')->insert(['name'=>$name, 'email'=>$email, 'phone'=>$phone, 'password'=>$newPassword]);
-                return response()->json(['http_code'=>'200','message'=>'Registro Existoso']);
+                return response()->json([
+                    'http_code'=>'200',
+                    'message'=>'Registro Existoso',
+                    'nombre' => $name,
+                    'correo' => $email,
+                    'telefono' => $phone,
+                    'contrasenia' => $passwor
+                ], 200);
             }
             
-        }else{
-            return response()->json(['http_code'=>'400','message'=>'Verifica tu password que coincidan']);
         }
+        // else{
+        //     return response()->json(['http_code'=>'500','message'=>'Verifica tu password que coincidan'],500);
+        // }
         
     }
 }
